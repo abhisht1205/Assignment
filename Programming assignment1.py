acbe60821a9829fb6671d392e93512150b2b4d21
abhisht1205 <117510407+abhisht1205@users.noreply.github.com>
2023-11-01 11:54:20 +0530

    Add files via upload

---
+++ Programming assignment1.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# coding: utf-8
+1. Write a Python program to print 'Hello Python'.
+# In[1]:
+
+
+print('Hello Python')
+
+2. Write a Python program to do arithmetical operations addition and division.?
+# In[2]:
+
+
+a = 10
+b = 2
+sum_ = a+b
+print("sum = ", sum_)
+div = a/b
+print("div = ",div)
+
+3. Write a Python program to find the area of a triangle?
+# In[3]:
+
+
+a = 20
+b = 15
+area = 1/2*a*b
+print("Area of the triangle is " , area)
+
+4. Write a Python program to swap two variables?
+# In[5]:
+
+
+a = 4
+b= 7
+c = a
+a=b
+b=c
+print(a,b)
+
+5. Write a Python program to generate a random number?
+# In[6]:
+
+
+import random
+
+random_number = random.randint(1,50)
+print("Random number" ,random_number )
+
+
+# In[ ]:
+
+
+
+
